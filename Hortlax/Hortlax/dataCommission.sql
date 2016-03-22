16494394362db2c4c52b6aa3b9fb4ff048d85e28
kim karlsson <k_karlsson@live.se>
2016-03-22 13:40:16 +0100

    commit insert commission

---
+++ Hortlax/Hortlax/dataCommission.sql
@@ -0,0 +1,17 @@
+﻿USE [TireChangeBusiness]
+GO
+
+INSERT INTO [dbo].[Commission]
+           (
+           [CommissionSize]
+           ,[CommissionDescription])
+     VALUES
+	 ( 3600, 'mer än 5 års anställning'),
+	 ( 1200, 'mer än 2 års anställning'),
+	 ( 200, 'steg 1, provision för väl utfört arbete'),
+	 ( 400, 'steg 2, provision för väl utfört arbete'),
+	 ( 800, 'steg 3, provision för väl utfört arbete')
+           --(<CommissionID, int,>
+           --,<CommiissionSize, money,>
+           --,<CommissionDescription, nvarchar(max),>)
+GO
